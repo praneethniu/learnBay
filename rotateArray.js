260b791e49808c92c0a3a83440671e70de32b233
Tangallapally <praneeth.tangallapal@dish.com>
2020-05-23 21:08:19 -0600

    [][][PATCH] more changes

--- rotateArray.js
+++ rotateArray.js
@@ -1,23 +1,12 @@
 const rotateArray = (nums, k) => {
-    if(nums.length === 1){
-        return nums
+    const a = []
+    for (let i = 0; i < nums.length; i++) {
+      a[(i + k) % nums.length] = nums[i];
     }
-    const len = nums.length
-    const delta = k > nums.length ? nums.length : k
-    for (let i = len-1; i>=0; i--) {
-      nums[i+delta] = nums[i]
-      console.log(nums)
+    for (let i = 0; i < nums.length; i++) {
+      nums[i] = a[i];
     }
-
-
-    for(let i=0; i<delta;i++){
-        nums[i] = nums[len+i]
-    }
-
-    for(let i=0; i<delta ;i++){
-        nums.pop()
-    }
-  return nums
+  }
 }
 
 const i = [1,2]
